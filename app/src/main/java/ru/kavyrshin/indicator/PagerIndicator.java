1de682d67f49854a2ebc0242d05030f1972e2c3c
Roman <rom.kawirshin@ya.ru>
2017-05-11 00:06:21 +0300

    Added color in view, added set and get methods

--- app/src/main/java/ru/kavyrshin/indicator/PagerIndicator.java
+++ app/src/main/java/ru/kavyrshin/indicator/PagerIndicator.java
@@ -2,6 +2,9 @@ package ru.kavyrshin.indicator;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
@@ -17,26 +20,39 @@ public class PagerIndicator extends View {
 
     private int countIndicator;
 
+    private int selectedColor;
+    private int unselectedColor;
+
+    private int currentPosition;
+
+    private Paint paint;
+
     public PagerIndicator(Context context) {
         super(context);
     }
 
     public PagerIndicator(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        paint = new Paint();
+
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PagerIndicator);
         countIndicator = typedArray.getInteger(R.styleable.PagerIndicator_pi_count, 0);
         dotRadiusPx = typedArray.getInteger(R.styleable.PagerIndicator_pi_radius, Util.dpToPx(DEFAULT_RADIUS));
         dotPaddingPx = typedArray.getInteger(R.styleable.PagerIndicator_pi_dotPadding, Util.dpToPx(DEFAULT_PADDING));
 
+        selectedColor = typedArray.getColor(R.styleable.PagerIndicator_pi_selectedColor, ContextCompat.getColor(getContext(), R.color.default_selected));
+        unselectedColor = typedArray.getColor(R.styleable.PagerIndicator_pi_unselectedColor, ContextCompat.getColor(getContext(), R.color.default_unselected));
+
         typedArray.recycle();
     }
 
     public PagerIndicator(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs);
     }
 
     public PagerIndicator(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs);
     }
 
     @Override
@@ -50,7 +66,7 @@ public class PagerIndicator extends View {
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         heightResult = MeasureSpec.getSize(heightMeasureSpec);
 
-        int necessaryWidthPx = (dotRadiusPx * 2 * countIndicator) + ((dotPaddingPx - 1) * countIndicator);
+        int necessaryWidthPx = (dotRadiusPx * 2 * countIndicator) + (dotPaddingPx * (countIndicator - 1));
         int necessaryHeightPx = dotRadiusPx * 2;
 
         if (widthMode == MeasureSpec.AT_MOST) {
@@ -67,5 +83,67 @@ public class PagerIndicator extends View {
 
 
         setMeasuredDimension(widthResult, heightResult);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawIndicator(canvas);
+    }
+
+    private void drawIndicator(Canvas canvas) {
+        paint.setColor(unselectedColor);
+
+        int step = (2 * dotRadiusPx) + dotPaddingPx;
+
+        int width = getWidth();
+        int height = getHeight();
+
+        for (int i = 0; i < countIndicator; i++) {
+
+            if (i == currentPosition) {
+                paint.setColor(selectedColor);
+            } else {
+                paint.setColor(unselectedColor);
+            }
+
+            int x = dotRadiusPx + (step * i);
+            canvas.drawCircle(x, height / 2, dotRadiusPx, paint);
+        }
+    }
+
+    public int getCountIndicator() {
+        return countIndicator;
+    }
+
+    public void setCountIndicator(int countIndicator) {
+        this.countIndicator = countIndicator;
+        invalidate();
+    }
+
+    public int getSelectedColor() {
+        return selectedColor;
+    }
+
+    public void setSelectedColor(int selectedColor) {
+        this.selectedColor = selectedColor;
+        invalidate();
+    }
+
+    public int getUnselectedColor() {
+        return unselectedColor;
+    }
+
+    public void setUnselectedColor(int unselectedColor) {
+        this.unselectedColor = unselectedColor;
+        invalidate();
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+        invalidate();
     }
 }
